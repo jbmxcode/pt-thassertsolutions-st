7170d27f729caca6dcfd3522c1a327018bd3edf7
josuebaloymelendez@gmail.com <josuebaloymelendez@gmail.com>
2020-12-19 18:27:12 -0500

    completed all styles for desktop

--- src/js/app.js
+++ src/js/app.js
@@ -6,3 +6,9 @@ import '../sass/app.scss';
 //////////////////////////
 // IMPORT LIBRARIES JS
 //////////////////////////
+
+document.querySelectorAll('.action-dots').forEach(item => {
+    item.addEventListener('click', event => {
+        item.nextElementSibling.classList.toggle('show')
+    })
+})
